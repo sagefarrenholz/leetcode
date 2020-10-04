e296ea0f224b11b779add8fc7cd72710402630df
sagefarrenholz <46610156+sagefarrenholz@users.noreply.github.com>
2020-10-04 02:05:05 -0400

    Optimized Add Two Numbers Creating additional list nodes was unneeded. Checking if both nodes will be null is unneeded, this condition never occurs.

--- AddTwoNumbers/cpp/AddTwoNumbers.cpp
+++ AddTwoNumbers/cpp/AddTwoNumbers.cpp
@@ -11,17 +11,14 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        if (l1 == nullptr && l2 == nullptr) return nullptr;
-        if (l1 == nullptr) return l2;
+	if (l1 == nullptr) return l2;
         if (l2 == nullptr) return l1;
-        ListNode* sum = new ListNode(l1->val + l2->val);
-        //if (sum->val >=)
-        sum->next = addTwoNumbers(l1->next, l2->next);
-        if (sum->val >= 10) {
-            sum->val -= 10;
-            if (sum->next == nullptr) sum->next = new ListNode(1);
-            else sum->next = addTwoNumbers(sum->next, new ListNode(1));
+        l1->val += l2->val;
+        l1->next = addTwoNumbers(l1->next, l2->next);
+        if (l1->val >= 10) {
+            l1->val -= 10;
+            l1->next = addTwoNumbers(l1->next, new ListNode(1));
         }
-        return sum;
-    }
+        return l1;
+     }
 };
--- AddTwoNumbers/cpp/README.md
+++ AddTwoNumbers/cpp/README.md
@@ -1,4 +1,4 @@
 Problem link: https://leetcode.com/problems/add-two-numbers
-Runtime: 32 ms, faster than 81.24% of C++ online submissions for Add Two Numbers.
-Memory Usage: 72.4 MB, less than 5.06% of C++ online submissions for Add Two Numbers.
-Submission link: https://leetcode.com/submissions/detail/404257583/
+Runtime: 20 ms, faster than 98.68% of C++ online submissions for Add Two Numbers.
+Memory Usage: 71.4 MB, less than 5.43% of C++ online submissions for Add Two Numbers.
+Submission link: https://leetcode.com/submissions/detail/404263768/
